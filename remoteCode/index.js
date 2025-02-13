82462ff8b2da4681c022459b199651d228f0fb84
gauravgautamapptile <gaurav.gautam@apptile.io>
2025-02-13 18:11:25 +0530

    add the plugins

--- remoteCode/index.js
+++ remoteCode/index.js
@@ -1,5 +1,9 @@
 // This file is generated. Do not edit.
+import webviewshoplazza from "./plugins/webviewshoplazza/source/widget";
+import shoplazzads from "./plugins/shoplazzads/source";
 export function initPlugins() {
 return [
+    shoplazzads,
+    webviewshoplazza,
   ];
 }
---
+++ remoteCode/plugins/shoplazzads/source/index.js
@@ -0,0 +1,198 @@
+import {
+  wrapDatasourceModel,
+  registerDatasource,
+  AjaxQueryRunner,
+} from 'apptile-core';
+import {datasourceModel, queryDetails as shoplazzaQueryDetails} from './ds';
+import {Platform} from 'react-native';
+
+const pluginListing = {
+  labelPrefix: 'shoplazzads',
+  type: 'datasource',
+  name: 'shoplazzads',
+  description: 'Shoplazza datasource',
+  section: 'SDK',
+  icon: 'datasource',
+  manifest: {
+    directoryName: 'shoplazzads',
+  },
+};
+const propertySettings = {};
+
+datasourceModel.name = pluginListing.name;
+datasourceModel.options.pluginListing = pluginListing;
+datasourceModel.options.propertySettings = propertySettings;
+
+const makeInputParamsResolver = contextInputParams => {
+  return (dsConfig, dsModelValues) => {
+    const dsPluginConfig = dsConfig.get('config');
+    if (!dsPluginConfig) {
+      return;
+    }
+
+    return Object.entries(contextInputParams).reduce((acc, [key, value]) => {
+      if (dsPluginConfig && dsPluginConfig.get(key)) {
+        return {...acc, [key]: dsPluginConfig.get(key)};
+      }
+      if (dsModelValues && dsModelValues.get(key)) {
+        return {...acc, [key]: dsModelValues.get(key)};
+      }
+      return acc;
+    }, {});
+  };
+};
+
+const makeInputVariablesTypeCompatible = (
+  inputVariables,
+  editableInputParams,
+) => {
+  return Object.entries(inputVariables).reduce((acc, [key, value]) => {
+    if (editableInputParams && editableInputParams[key] !== undefined) {
+      if (typeof editableInputParams[key] === 'number') {
+        return {
+          ...acc,
+          [key]:
+            isNaN(value) && typeof value === typeof editableInputParams[key]
+              ? value
+              : parseInt(value),
+        };
+      } else {
+        return value
+          ? {
+              ...acc,
+              [key]: value,
+            }
+          : acc;
+      }
+    }
+    return acc;
+  }, {});
+};
+
+const executeQuery = async (
+  dsModel,
+  dsConfig,
+  dsModelValues,
+  queryDetails,
+  inputVariables,
+  options,
+) => {
+  try {
+    let {endpointResolver, contextInputParams, editableInputParams, endpoint} =
+      queryDetails ?? {};
+    const contextInputParamsResolver =
+      makeInputParamsResolver(contextInputParams);
+    const contextInputVariables = contextInputParamsResolver(
+      dsConfig,
+      dsModelValues,
+    );
+
+    let typedInputVariables, typedDataVariables;
+    if (editableInputParams) {
+      typedInputVariables = makeInputVariablesTypeCompatible(
+        inputVariables,
+        editableInputParams,
+      );
+    }
+
+    endpoint =
+      endpointResolver &&
+      endpointResolver(endpoint, typedInputVariables, contextInputVariables);
+
+    typedDataVariables = queryDetails.inputResolver
+      ? queryDetails.inputResolver(typedInputVariables)
+      : typedInputVariables;
+
+    const apiBaseUrl = queryDetails.apiBaseUrlResolver(dsModelValues);
+    const serverApiBaseUrl =
+      queryDetails.serverApiBaseUrlResolver(dsModelValues);
+
+    let headers = {};
+    if (queryDetails.queryHeadersResolver) {
+      headers = queryDetails.queryHeadersResolver(
+        inputVariables,
+        contextInputVariables,
+      );
+    }
+
+    let queryRunner = AjaxQueryRunner();
+
+    const queryResponse = {data: {}};
+    queryResponse.data =
+      Platform.OS === 'web'
+        ? await fetch(`${serverApiBaseUrl}api/themes/`, {
+            headers: {'x-base-url': apiBaseUrl + endpoint},
+          }).then(res => res.json())
+        : await fetch(apiBaseUrl + endpoint).then(res => res.json());
+    const rawData =
+      queryResponse && queryResponse.data ? queryResponse.data : {};
+    let transformedData = rawData;
+    let queryHasNextPage, paginationDetails;
+    if (queryDetails && queryDetails.transformer) {
+      const {
+        data,
+        hasNextPage,
+        paginationMeta: pageData,
+      } = queryDetails.transformer(rawData, null, dsModelValues);
+
+      transformedData = data;
+      queryHasNextPage = hasNextPage;
+      paginationDetails = pageData;
+    }
+
+    return {
+      rawData,
+      data: transformedData,
+      hasNextPage: queryHasNextPage,
+      paginationMeta: paginationDetails,
+      errors: [],
+      hasError: false,
+    };
+  } catch (ex) {
+    const errors = ex?.response?.data?.errors || [ex?.message];
+    return {
+      rawData: {},
+      data: {},
+      hasNextPage: false,
+      paginationMeta: {},
+      errors: errors,
+      hasError: true,
+    };
+  }
+};
+
+datasourceModel.getQueries = () => shoplazzaQueryDetails;
+datasourceModel.getPlatformIdentifier = 'shoplazza';
+datasourceModel.getQueryInputParams = function (queryName) {
+  const queryDetails =
+    shoplazzaQueryDetails && shoplazzaQueryDetails[queryName]
+      ? shoplazzaQueryDetails[queryName]
+      : null;
+  return queryDetails?.editableInputParams
+    ? queryDetails?.editableInputParams
+    : {};
+};
+datasourceModel.runQuery = function* (
+  dsModel,
+  dsConfig,
+  dsModelValues,
+  queryName,
+  inputVariables,
+  options,
+) {
+  if (!shoplazzaQueryDetails[queryName]) {
+    return;
+  }
+  return yield executeQuery(
+    dsModel,
+    dsConfig,
+    dsModelValues,
+    shoplazzaQueryDetails[queryName],
+    inputVariables,
+    options,
+  );
+};
+
+const shoplazzaDatasourceModel = wrapDatasourceModel(datasourceModel);
+registerDatasource(shoplazzaDatasourceModel.name, shoplazzaDatasourceModel);
+export default shoplazzaDatasourceModel;
