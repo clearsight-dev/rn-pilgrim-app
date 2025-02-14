e9f0888350291c5b42545cac7bd05c9dc3462840
gauravgautamapptile <gaurav.gautam@apptile.io>
2025-02-14 16:42:27 +0530

    Remove stuff specific to shoplazza

--- remoteCode/index.js
+++ remoteCode/index.js
@@ -1,9 +1,5 @@
 // This file is generated. Do not edit.
-import webviewshoplazza from "./plugins/webviewshoplazza/source/widget";
-import shoplazzads from "./plugins/shoplazzads/source";
 export function initPlugins() {
 return [
-    shoplazzads,
-    webviewshoplazza,
   ];
 }
