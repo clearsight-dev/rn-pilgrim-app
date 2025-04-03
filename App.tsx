ba88454c98b9f785391cecebaa2f0d9210ed8573
gauravgautamapptile <gaurav.gautam@apptile.io>
2025-04-03 18:40:22 +0530

    Optimization trailing

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import {
   apptileNavigationRef,
   ApptileWrapper,
   ApptileAppRoot,
-  useStartApptile
+  useStartApptile,
+  ApolloQueryRunner
 } from 'apptile-core';
 
 import JSSplash from './components/JSSplash';
@@ -29,60 +30,40 @@ function App(): React.JSX.Element {
   const [pilgrimGlobals, setPilgrimGlobals] = useState({homePageScrolledDown: false});
   const status = useStartApptile(initAnalytics);
 
-  let body = null;
-  if (status.isDownloading) {
-    body = <JSSplash />;
-  } else {
-    body = (
-      <NavigationContainer
-        ref={apptileNavigationRef}
-        theme={{
-          ...DefaultTheme,
-          colors: status.theme
-        }}
-        linking={status.linking}
-        onReady={() => {
-          console.log("Navigators are ready")
-          // RNApptile.notifyJSReady();
+  let body = (
+    <NavigationContainer
+      ref={apptileNavigationRef}
+      theme={{
+        ...DefaultTheme,
+        colors: status.theme
+      }}
+      linking={status.linking}
+      onReady={() => {
+        console.log("Navigators are ready")
+        // RNApptile.notifyJSReady();
+      }}
+    >
+      <Stack.Navigator
+        screenOptions={{
+          animation: 'none'
         }}
       >
-        <Stack.Navigator
-          screenOptions={{
-            animation: 'none'
-          }}
-        >
-          <Stack.Screen name="NocodeRoot" component={ApptileAppRoot} options={{ headerShown: false }} />
-          <Stack.Screen
-            name="NativeUtils"
-            component={UpdateModal}
-            options={{ headerShown: true }}
-            initialParams={{ appId: status.appId }}
-          />
-          <Stack.Screen
-            name="AdminPage"
-            component={AdminPage}
-            options={{ headerShown: true }}
-            initialParams={{ appId: status.appId }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-
-  let updateModal = null;
-  if (status.hasUpdate === "yes") {
-    // Note that you can use status.updateDownloaded === "yes" to show this, 
-    // if you want to wait till the update is fully downloaded. In that case the
-    // app will restart as soon as the modal is dismissed
-    // If you want to show the update modal as soon as you have determined that there
-    // is an update and want the download to happen while the modal is being 
-    // displayed then use the status.hasUpdate
-    updateModal = (<FloatingUpdateModal
-      navigationRef={apptileNavigationRef}
-      appId={status.appId}
-      updateDownloaded={status.updateDownloaded}
-    />);
-  }
+        <Stack.Screen name="NocodeRoot" component={ApptileAppRoot} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="NativeUtils"
+          component={UpdateModal}
+          options={{ headerShown: true }}
+          initialParams={{ appId: status.appId }}
+        />
+        <Stack.Screen
+          name="AdminPage"
+          component={AdminPage}
+          options={{ headerShown: true }}
+          initialParams={{ appId: status.appId }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 
   // The nocode layer will not do navigation to these screens so you can handle those navigations in the onNavigationEvent
   return (
@@ -95,7 +76,7 @@ function App(): React.JSX.Element {
         }}
       >
         {body}
-        {updateModal}
+        {(!status.modelReady) && <JSSplash/>}
       </ApptileWrapper>
     </PilgrimContext.Provider>
   );
