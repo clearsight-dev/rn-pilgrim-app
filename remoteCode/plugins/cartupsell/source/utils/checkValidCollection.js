df64d963918dd5b18d487c03e922ae1b9ffa2309
Ibtesam.Ansari <ibtesam.ansari@apptile.io>
2025-08-18 20:13:30 +0530

    updated validation check

--- remoteCode/plugins/cartupsell/source/utils/checkValidCollection.js
+++ remoteCode/plugins/cartupsell/source/utils/checkValidCollection.js
@@ -2,6 +2,11 @@ export const checkValidCollection = (
   productCollectionIds,
   validCollectionIds,
 ) => {
+  // If no collections specified, rule applies to all products
+  if (!validCollectionIds || validCollectionIds.length === 0) {
+    return true;
+  }
+
   return productCollectionIds.some(collectionId =>
     validCollectionIds.includes(collectionId),
   );
